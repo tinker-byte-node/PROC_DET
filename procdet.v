7daee6d543b4d3c9b479c69446a4338a58a6843f
Arjun Raj <tinkerbytenode@gmail.com>
2026-08-03 22:05:19 +0530

    d

--- procdet.v
+++ procdet.v
@@ -1,2 +1,2 @@
 https://chipsandcheese.com/p/inside-sifives-p550-microarchitecture
-https://ieeexplore.ieee.org/document/7924241
+https://ieeexplore.ieee.org/document/7924241 https://ieeexplore.ieee.org/document/9352481
