cfc36b4543dfeb91ce3ef85787c310f62e5136fc
Arjun Raj <tinkerbytenode@gmail.com>
2026-08-03 22:06:41 +0530

    Update procdet.v

--- procdet.v
+++ procdet.v
@@ -1,2 +1,7 @@
 https://chipsandcheese.com/p/inside-sifives-p550-microarchitecture
-https://ieeexplore.ieee.org/document/7924241 https://ieeexplore.ieee.org/document/9352481
+https://ieeexplore.ieee.org/document/7924241 
+
+/////IBM POWER 10
+https://ieeexplore.ieee.org/document/9352481
+
+https://ieeexplore.ieee.org/document/9731594
